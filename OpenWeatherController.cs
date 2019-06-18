2b1b77251783da3f55048f5c4170dce0d1361d3d
Tom Mein <tomandclairemein@gmail.com>
2019-06-18 16:55:18 +0300

    Update OpenWeatherController.cs

--- OpenWeatherController.cs
+++ OpenWeatherController.cs
@@ -1,12 +1,12 @@
 using ChartJSCore.Models;
-using CretanVillas2019.Models;
+using YourSolutionName.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CretanVillas2019.Controllers
+namespace YourSolutionName.Controllers
 {
 
     public class OpenWeatherController : Controller
